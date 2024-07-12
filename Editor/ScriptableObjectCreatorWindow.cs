ff58c3d498cf8ed89af2db768df1d09ecaa7a07f
TRS <118838203+trs-game-tech@users.noreply.github.com>
2024-07-12 22:28:26 +0900

    #if UNITY_EDITOR

--- Editor/ScriptableObjectCreatorWindow.cs
+++ Editor/ScriptableObjectCreatorWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+#if UNITY_EDITOR
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
@@ -229,3 +230,4 @@ public sealed class ScriptableObjectCreatorWindow : EditorWindow
         return true;
     }
 }
+#endif
